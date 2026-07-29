5c5d9c1a01352d257aadb1e9419ce1343896fc86
hatemelbadwy <hatemelbadwy10@gmail.com>
2026-07-30 01:44:57 +0300

    remove obsolete dependency registrations

--- lib/data/config/di.dart
+++ lib/data/config/di.dart
@@ -69,9 +69,7 @@ Future<void> init() async {
     ),
   );
 
-  // /// Repository
-  // sl.registerLazySingleton(() => LocalizationRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
+  // Repositories
   sl.registerLazySingleton(
       () => HomeRepo(sharedPreferences: sl(), dioClient: sl()));
   sl.registerLazySingleton(
@@ -100,10 +98,8 @@ Future<void> init() async {
       () => ChatRepo(sharedPreferences: sl(), dioClient: sl()));
   sl.registerLazySingleton(
       () => SplashRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
   sl.registerLazySingleton(
       () => ProjectsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
   sl.registerLazySingleton(
       () => LoginRepo(sharedPreferences: sl(), dioClient: sl()));
   sl.registerLazySingleton<AuthSessionStore>(
@@ -112,16 +108,12 @@ Future<void> init() async {
       dioClient: sl(),
     ),
   );
-  //
   sl.registerLazySingleton(
       () => RegisterRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
   sl.registerLazySingleton(
       () => ConfirmCodeRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
   sl.registerLazySingleton(
       () => SendVerificationRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
   sl.registerLazySingleton(
       () => ChangePasswordRepo(sharedPreferences: sl(), dioClient: sl()));
   sl.registerFactory(() => AddProjectBloc(repository: sl()));
@@ -153,101 +145,6 @@ Future<void> init() async {
       subscriptionController: sl<UserChannelRealtimeService>(),
     ),
   );
-
-  //
-  // sl.registerLazySingleton(() => ChangePasswordRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => LogoutRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => ActivationAccountRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => ProfileRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => EditProfileRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => MapsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(
-  //     () => SocialMediaRepo(sharedPreferences: sl(), dioClient: sl(), socialMediaLoginHelper: sl()));
-  //
-  // sl.registerLazySingleton(() => DeactivateAccountRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => ServicesRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => OffersRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => PartnersRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => NewsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => NewsDetailsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => NotificationsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => BannersRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => MembershipRequestsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => AddMembershipRequestRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => AddStudentMembershipRequestRepo(sharedPreferences: sl(), dioClient: sl()));
-  // sl.registerLazySingleton(() => AddAssociationRequestRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => UpdateGeneralDataRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => EducationalDataRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => AddEducationalDataRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => ExperienceDataRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => AddExperienceDataRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => CompleteMembershipRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => SubscriptionsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => SubscribeRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => ContactWithUsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => BoardMembersRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => MembershipInstructionsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => CoursesRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => MyCoursesRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => CourseDetailsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => AssociationsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => MyAssociationsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => AssociationDetailsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => ConferencesRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => MyConferencesRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => ConferenceDetailsRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => AddConferenceRequestRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // sl.registerLazySingleton(() => CheckOutRepo(sharedPreferences: sl(), dioClient: sl()));
-  //
-  // //provider
-  // sl.registerLazySingleton(() => LanguageBloc(repo: sl()));
-  // sl.registerLazySingleton(() => ThemeProvider(sharedPreferences: sl()));
-  // sl.registerLazySingleton(() => DashboardBloc(repo: sl()));
-  // sl.registerLazySingleton(() => ProfileBloc(repo: sl()));
-  // sl.registerLazySingleton(() => UserBloc(repo: sl()));
-  // sl.registerLazySingleton(() => BannersBloc(repo: sl(), internetConnection: sl()));
-  // sl.registerLazySingleton(() => MembershipInstructionsBloc(repo: sl(), internetConnection: sl()));
-  //
-  // ///Log out
-  // sl.registerLazySingleton(() => LogoutBloc(repo: sl()));
 
   // External
   final sharedPreferences = await SharedPreferences.getInstance();
